7bda527356a7946584f72f4acfe0af1a3c26f8a4
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-21 16:47:16 -0700

    Removed unecessary comments

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -2,8 +2,6 @@
 export default function UserForm() {
     return (
       <>
-
-  
         <div className="hidden sm:block" aria-hidden="true">
           <div className="py-5">
             <div className="border-t border-gray-200" />
@@ -31,21 +29,16 @@ export default function UserForm() {
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
-  
+                      
                       <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                          Choose Workflow
-                        </label>
-                        <select
-                          id="country"
-                          name="country"
-                          autoComplete="country"
-                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        >
-                          <option>Create Opportunity</option>
-                          <option></option>
-                          <option></option>
-                        </select>
+                        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                          <button
+                            type="submit"
+                            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          >
+                            Define Criteria
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
