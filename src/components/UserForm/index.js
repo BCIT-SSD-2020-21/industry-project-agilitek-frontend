1b31b22d845a5ace6153ef50d59fcd8ed0157437
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-21 16:05:42 -0700

    adjusting layout

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -142,18 +142,20 @@ export default function UserForm() {
   
         <div className="mt-10 sm:mt-0">
           <div className="md:grid md:grid-cols-3 md:gap-6">
+
             <div className="md:col-span-1">
-              <div className="px-4 sm:px-0">
+              {/* <div className="px-4 sm:px-0">
                 <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
                 <p className="mt-1 text-sm text-gray-600">Use a permanent address where you can receive mail.</p>
-              </div>
+              </div> */}
             </div>
+
             <div className="mt-5 md:mt-0 md:col-span-2">
               <form action="#" method="POST">
                 <div className="shadow overflow-hidden sm:rounded-md">
                   <div className="px-4 py-5 bg-white sm:p-6">
                     <div className="grid grid-cols-6 gap-6">
-                      <div className="col-span-6 sm:col-span-3">
+                      {/* <div className="col-span-6 sm:col-span-3">
                         <label htmlFor="first_name" className="block text-sm font-medium text-gray-700">
                           First name
                         </label>
@@ -177,24 +179,25 @@ export default function UserForm() {
                           autoComplete="family-name"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
-                      </div>
+                      </div> */}
   
                       <div className="col-span-6 sm:col-span-4">
                         <label htmlFor="email_address" className="block text-sm font-medium text-gray-700">
-                          Email address
+                          SQL QUERY
                         </label>
                         <input
                           type="text"
                           name="email_address"
                           id="email_address"
                           autoComplete="email"
+                          placeholder="SQL Query"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
   
                       <div className="col-span-6 sm:col-span-3">
                         <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                          Country / Region
+                          Choose Workflow
                         </label>
                         <select
                           id="country"
@@ -202,9 +205,9 @@ export default function UserForm() {
                           autoComplete="country"
                           className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         >
-                          <option>United States</option>
-                          <option>Canada</option>
-                          <option>Mexico</option>
+                          <option>Create Opportunity</option>
+                          <option></option>
+                          <option></option>
                         </select>
                       </div>
   
@@ -233,7 +236,7 @@ export default function UserForm() {
                         />
                       </div>
   
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
                         <label htmlFor="state" className="block text-sm font-medium text-gray-700">
                           State / Province
                         </label>
@@ -243,9 +246,9 @@ export default function UserForm() {
                           id="state"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
-                      </div>
+                      </div> */}
   
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                      {/* <div className="col-span-6 sm:col-span-3 lg:col-span-2">
                         <label htmlFor="postal_code" className="block text-sm font-medium text-gray-700">
                           ZIP / Postal
                         </label>
@@ -256,7 +259,7 @@ export default function UserForm() {
                           autoComplete="postal-code"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                   <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
