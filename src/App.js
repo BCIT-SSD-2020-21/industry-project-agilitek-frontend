a943a471388819bae66f4ad9a993a2715789cff3
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-21 15:44:09 -0700

    sucessfully added userform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MainPage from './components/MainPage'
+import UserForm from './components/MainPage/UserForm'
 
 
 function App() {
   return (
    <>
   <MainPage></MainPage>
+  <UserForm></UserForm>
    </>
   );
 }
