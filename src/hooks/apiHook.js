bfd9d676823eaa537971ca51b242972901f96787
Leanne Sall <lsall1@my.bcit.ca>
2021-04-22 15:56:26 -0700

    testing and cleaned up hook

--- src/hooks/apiHook.js
+++ src/hooks/apiHook.js
@@ -2,21 +2,46 @@ import axios from "axios";
 import React, { useState, useEffect, useContext, createContext } from "react";
 
 // Dashboard: Get user current workflows
-export function getCurrentWorkFlows() {
-  axios.get("AWSUSERWORKFLOWS").then((res) => {
-    return res.data;
-  });
+export function getCurrentWorkFlows(id) {
+  axios
+    .get(
+      "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod/myworkflows"
+    )
+    .then((res) => {
+      return res.data;
+    });
 }
 // UseEffect/PageLoad add new workflow: Request for input list of specific Workflow
 export function addSalesForceFlow() {
-  axios.get("AWSWORKFLOWINPUT").then((res) => {
-    return res.data;
-  });
+  axios
+    .get(
+      "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod/salesforceflows"
+    )
+    .then((res) => {
+      return res.data;
+    });
 }
 // onAction/Button: Need a request to send the SQL query as well as well as the workflow &&
 //    request to store new workflow in data in database
 export function setQuery(name, query, workFlow) {
-  axios.post("AWSTRIGGERSALESFORCE", query, workFlow).then(() => {});
+  axios
+    .post(
+      "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod/salesforceflows"
+      // query,
+      // workFlow
+    )
+    .then((res) => {
+      return res.data;
+    });
 
-  axios.post("AWSUSERWORKFLOWS", name, query, workflow).then(() => {});
+  axios
+    .post(
+      "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod/myworkflows"
+      // name,
+      // query,
+      // workflow
+    )
+    .then((res) => {
+      return res.data;
+    });
 }
