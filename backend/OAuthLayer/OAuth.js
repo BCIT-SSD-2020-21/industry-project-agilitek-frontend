2c8aa8b37adac9b697e52f663f21db343fb027ec
Steven <clai86@my.bcit.ca>
2021-04-22 12:17:18 -0700

    Wrap code in getOAuthToken function

--- backend/OAuthLayer/OAuth.js
+++ backend/OAuthLayer/OAuth.js
@@ -1,34 +1,38 @@
 const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 
-module.exports = async () => {
-    try {
-        // Setup body object
-        const params = new URLSearchParams();
-        params.append('username', process.env.USERNAME);
-        params.append(
-            'password',
-            `${process.env.PASSWORD}${process.env.SECURITY_TOKEN}`
-        );
-        params.append('grant_type', process.env.GRANT_TYPE);
-        params.append('client_id', process.env.CLIENT_ID);
-        params.append('client_secret', process.env.CLIENT_SECRET);
+module.exports = function () {
+    async function getOAuthToken() {
+        try {
+            // Setup body object
+            const params = new URLSearchParams();
+            params.append('username', process.env.USERNAME);
+            params.append(
+                'password',
+                `${process.env.PASSWORD}${process.env.SECURITY_TOKEN}`
+            );
+            params.append('grant_type', process.env.GRANT_TYPE);
+            params.append('client_id', process.env.CLIENT_ID);
+            params.append('client_secret', process.env.CLIENT_SECRET);
 
-        // Get the OAuth token
-        const res = await fetch(
-            `${process.env.INSTANCE_URL}/services/oauth2/token`,
-            {
-                method: 'POST',
-                body: params,
+            // Get the OAuth token
+            const res = await fetch(
+                `${process.env.INSTANCE_URL}/services/oauth2/token`,
+                {
+                    method: 'POST',
+                    body: params,
+                }
+            );
+            const data = await res.json();
+
+            // Check if data is undefined
+            if (data) {
+                return data.access_token;
             }
-        );
-        const data = await res.json();
-
-        // Check if data is undefined
-        if (data) {
-            return data.access_token;
+        } catch (error) {
+            throw Error(`Error while getting an OAuth 2.0 token: ${error}`);
         }
-    } catch (error) {
-        throw Error(`Error while getting an OAuth 2.0 token: ${error}`);
     }
+
+    return { getOAuthToken };
 };
