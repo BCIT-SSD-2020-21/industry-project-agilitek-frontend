a076243376ad7db330bd163743fbdfb4db7435ed
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-22 11:48:47 -0700

    renamed linking

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -47,7 +47,7 @@ function classNames(...classes) {
                               <Fragment>
                                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                                 <a href="" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
-                                    <Link to="/workflow">Work Flows</Link>
+                                    <Link to="/config">WorkFlow Configuration</Link>
                                 </a>
                               </Fragment>
                         </div>
@@ -187,12 +187,6 @@ function classNames(...classes) {
         </header>
         </div>
         </>
-
-
-
-
-
-        
     )
 }
 
