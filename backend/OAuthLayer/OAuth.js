dbc1fc61192a665abd289d19672eb4dfcd8feca8
Steven <clai86@my.bcit.ca>
2021-04-22 15:09:37 -0700

    Modify OAuth.js

--- backend/OAuthLayer/OAuth.js
+++ backend/OAuthLayer/OAuth.js
@@ -1,20 +1,9 @@
 const fetch = require('node-fetch');
-const { URLSearchParams } = require('url');
+const qs = require('qs');
 
 module.exports = function () {
     async function getOAuthToken() {
         try {
-            // Setup body object
-            const params = new URLSearchParams();
-            params.append('username', process.env.USERNAME);
-            params.append(
-                'password',
-                process.env.PASSWORD + process.env.SECURITY_TOKEN
-            );
-            params.append('grant_type', process.env.GRANT_TYPE);
-            params.append('client_id', process.env.CLIENT_ID);
-            params.append('client_secret', process.env.CLIENT_SECRET);
-
             // Get the OAuth token
             const res = await fetch(
                 `${process.env.INSTANCE_URL}/services/oauth2/token`,
@@ -23,7 +12,13 @@ module.exports = function () {
                     headers: {
                         'Content-Type': 'application/x-www-form-urlencoded',
                     },
-                    body: params,
+                    body: qs.stringify({
+                        username: process.env.USERNAME,
+                        password: `${process.env.PASSWORD}${process.env.SECURITY_TOKEN}`,
+                        grant_type: process.env.GRANT_TYPE,
+                        client_id: process.env.CLIENT_ID,
+                        client_secret: process.env.CLIENT_SECRET,
+                    }),
                 }
             );
             const data = await res.json();
