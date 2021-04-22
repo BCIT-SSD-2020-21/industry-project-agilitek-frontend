6d6d85fb6b9592216f488465e8adc7eb5277a18f
Steven <clai86@my.bcit.ca>
2021-04-22 11:28:10 -0700

    Add try catch

--- backend/OAuthLayer/index.js
+++ backend/OAuthLayer/index.js
@@ -1,24 +1,32 @@
 const fetch = require('node-fetch');
 
 module.exports = async () => {
-    const body = {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-        grant_type: process.env.GRANT_TYPE,
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-    };
+    try {
+        const body = {
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD,
+            grant_type: process.env.GRANT_TYPE,
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+        };
 
-    const res = await fetch(
-        `${process.env.INSTANCE_URL}/services/oauth2/token`,
-        {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: JSON.stringify(body),
+        const res = await fetch(
+            `${process.env.INSTANCE_URL}/services/oauth2/token`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(body),
+            }
+        );
+        const data = await res.json();
+
+        // Check if data is undefined
+        if (data) {
+            return data.access_token;
         }
-    );
-
-    const data = await res.json();
-
-    return data.access_token;
+    } catch (error) {
+        throw Error(`Error while getting an OAuth 2.0 token: ${error}`);
+    }
 };
