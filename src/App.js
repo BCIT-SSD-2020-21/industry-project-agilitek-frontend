a50c527cf8f23ba566e5e84fc3ffa2cb0888c691
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-22 10:39:15 -0700

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React from 'react'
+import Navbar from './components/Navbar'
 import MainPage from './components/MainPage'
-import UserForm from './components/UserForm'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
    <>
-  <MainPage></MainPage>
-  {/* <UserForm></UserForm> */}
+    <Router>
+    <div className="bg-gray-800 pb-32">
+      <Navbar/>
+      <Switch>
+        <Route path="/workflow">
+          <></>
+        </Route>
+        <Route path="/">
+          <MainPage></MainPage>
+        </Route>
+      </Switch>
+      </div>
+    </Router>
    </>
   );
 }
