36785608e4dc72f9b93a9c211edfd0f344d18210
Leanne Sall <lsall1@my.bcit.ca>
2021-04-22 15:07:56 -0700

    cleaned up files and switched hook

--- src/hooks/apiHook.js
+++ src/hooks/apiHook.js
@@ -1,28 +1,22 @@
 import axios from "axios";
 import React, { useState, useEffect, useContext, createContext } from "react";
 
-//create context
-const apiContext = createContext();
+// Dashboard: Get user current workflows
+export function getCurrentWorkFlows() {
+  axios.get("AWSUSERWORKFLOWS").then((res) => {
+    return res.data;
+  });
+}
+// UseEffect/PageLoad add new workflow: Request for input list of specific Workflow
+export function addSalesForceFlow() {
+  axios.get("AWSWORKFLOWINPUT").then((res) => {
+    return res.data;
+  });
+}
+// onAction/Button: Need a request to send the SQL query as well as well as the workflow &&
+//    request to store new workflow in data in database
+export function setQuery(name, query, workFlow) {
+  axios.post("AWSTRIGGERSALESFORCE", query, workFlow).then(() => {});
 
-export const useContext = () => {
-  return useContext(apiContext);
-};
-
-export const getProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-
-  // Dashboard: Get user current workflows
-  // UseEffect add new workflow: Request for input list of specific Workflow
-  // Button: Need a request to send the SQL query as well as well as the workflow &&
-  //    request to store new workflow in data in database
-
-  function getId(id) {
-    axios.get("");
-  }
-
-  const value = {
-    signup,
-  };
-
-  return <ApiContext.Provider value={value}>{children}</ApiContext.Provider>;
-};
+  axios.post("AWSUSERWORKFLOWS", name, query, workflow).then(() => {});
+}
