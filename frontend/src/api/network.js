f44155ca5517499cf1f34946fffa364f3e382f12
Gökay Abay <abaygokay@gmail.com>
2021-04-23 13:18:15 -0700

    turn funcs to async await, add more methods

--- frontend/src/api/network.js
+++ frontend/src/api/network.js
@@ -1,27 +1,48 @@
 import axios from "axios"
 import React, { useState, useEffect, useContext, createContext } from "react"
 
-// Dashboard: Get user current workflows
-export function getCurrentWorkFlows(id) {
-  axios
-    .get(
-      "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod/myworkflows"
-    )
-    .then((res) => {
-      return res.data
-    })
+const url = "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod"
+
+// Dashboard: Get all user workflows
+export async function getAllWorkflows() {
+  try {
+    const res = await axios.get(`${url}/myworkflows`)
+    return res.data
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+// Dashboard: Get user workflow by id
+export async function getWorkflow(id) {
+  try {
+    const res = await axios.get(`${url}/myworkflows/${id}`)
+    return res.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// Create a Workflow
+export async function createWorkflow() {
+  try {
+    const res = await axios.post(`${url}/myworkflows/${id}`)
+    return res.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 // UseEffect/PageLoad add new workflow: Request for input list of specific Workflow
 export async function getSalesForceFlow() {
   try {
-    const res = await axios.get(
-      "https://jzqdyrxgy2.execute-api.us-east-1.amazonaws.com/prod/salesforceflows"
-    )
+    const res = await axios.get(`${url}/salesforceflows`)
     return res.data.actions
   } catch (err) {
     console.log(err)
   }
 }
+
 // onAction/Button: Need a request to send the SQL query as well as well as the workflow &&
 //    request to store new workflow in data in database
 export function setQuery(name, query, workFlow) {
