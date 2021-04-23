b11aab45a73349b896eda7a76b4f97115e83d68d
Steven <clai86@my.bcit.ca>
2021-04-23 13:36:32 -0700

    Finish triggerSalesforceFlow lambda

--- backend/TriggerSalesforceFlow/index.js
+++ backend/TriggerSalesforceFlow/index.js
@@ -2,18 +2,8 @@ const OAuth = require('/opt/OAuth');
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-    var endpoint = '';
-    var method = '';
-    var inputs;
-
     // Getting the body
-    if (event.body !== null && event.body !== undefined) {
-        let body = JSON.parse(event.body);
-
-        endpoint = body.endpoint;
-        method = body.method;
-        inputs = body.inputs;
-    }
+    const { endpoint, method, inputs } = event;
 
     try {
         // Getting the OAuth token
@@ -21,18 +11,40 @@ exports.handler = async (event) => {
         const OAuthData = await OAuthRes.getOAuthToken();
 
         // HTTP request on Salesforce endpoint
-        const response = await fetch(`${INSTANCE_URL}${endpoint}`, {
-            method,
-            headers: {
-                Authorization: `Bearer ${OAuthData.access_token}`,
-            },
-            body: {
-                inputs,
-            },
-        });
+        var response = {};
+
+        if (method.toLowerCase() === 'get') {
+            response = await fetch(
+                `${process.env.INSTANCE_URL}${endpoint}?inputs=${inputs}`,
+                {
+                    method,
+                    headers: {
+                        Authorization: `Bearer ${OAuthData.access_token}`,
+                    },
+                }
+            );
+        } else if (method.toLowerCase() === 'post') {
+            response = await fetch(`${process.env.INSTANCE_URL}${endpoint}`, {
+                method,
+                headers: {
+                    Authorization: `Bearer ${OAuthData.access_token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    inputs,
+                }),
+            });
+        }
+
         const data = await response.json();
 
-        return data;
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(data),
+        };
     } catch (error) {
         throw Error(`Error while requesting Salesforce API: ${error}`);
     }
