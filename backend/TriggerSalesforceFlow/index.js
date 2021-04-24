983e18c2749b623b8f3d94bfa68994c8de320562
Steven <clai86@my.bcit.ca>
2021-04-23 17:19:37 -0700

    Finish modify trigger salesforce flow lambda

--- backend/TriggerSalesforceFlow/index.js
+++ backend/TriggerSalesforceFlow/index.js
@@ -3,47 +3,67 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
     // Getting the body
-    const { endpoint, method, inputs } = event;
+    const { endpoint, inputs } = event;
+    var label = '';
 
     try {
         // Getting the OAuth token
         const OAuthRes = await OAuth();
         const OAuthData = await OAuthRes.getOAuthToken();
 
-        // HTTP request on Salesforce endpoint
-        var response = {};
+        // Get flow label
+        const getLabelResponse = await fetch(
+            `${process.env.INSTANCE_URL}${endpoint}`,
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${OAuthData.access_token}`,
+                },
+            }
+        );
+        const getLabelData = await getLabelResponse.json();
 
-        if (method.toLowerCase() === 'get') {
-            response = await fetch(
-                `${process.env.INSTANCE_URL}${endpoint}?inputs=${inputs}`,
-                {
-                    method,
-                    headers: {
-                        Authorization: `Bearer ${OAuthData.access_token}`,
-                    },
-                }
-            );
-        } else if (method.toLowerCase() === 'post') {
-            response = await fetch(`${process.env.INSTANCE_URL}${endpoint}`, {
-                method,
+        // Check if label exists
+        // If exists then assign the label
+        // Else return an error message
+        if (
+            getLabelData &&
+            getLabelData.inputs[0] &&
+            getLabelData.inputs[0].label
+        ) {
+            label = getLabelData.inputs[0].label;
+        } else {
+            return {
+                statusCode: 400,
+                'Access-Control-Allow-Origin': '*',
+                body: JSON.stringify({
+                    errorMessage: 'Label not found',
+                }),
+            };
+        }
+
+        // Post request to Salesforce API
+        const postResponse = await fetch(
+            `${process.env.INSTANCE_URL}${endpoint}`,
+            {
+                method: 'POST',
                 headers: {
                     Authorization: `Bearer ${OAuthData.access_token}`,
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    inputs,
+                    inputs: [{ [label]: inputs }],
                 }),
-            });
-        }
-
-        const data = await response.json();
+            }
+        );
+        const postData = await postResponse.json();
 
         return {
             statusCode: 200,
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(postData),
         };
     } catch (error) {
         throw Error(`Error while requesting Salesforce API: ${error}`);
