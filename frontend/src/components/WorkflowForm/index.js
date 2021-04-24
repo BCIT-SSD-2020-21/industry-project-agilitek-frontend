8d6a85035cc4a7b264e1be64205c86264d74a690
Gökay Abay <abaygokay@gmail.com>
2021-04-24 11:47:30 -0700

    Add fields to Workflow form

--- frontend/src/components/WorkflowForm/index.js
+++ frontend/src/components/WorkflowForm/index.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react"
 import { getSalesForceFlow, setQuery } from "../../api/network"
+import { Switch } from "@headlessui/react"
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
 
 const people = [
   {
@@ -14,6 +19,7 @@ const people = [
 export default function UserForm() {
   const [options, setOptions] = useState([])
   const [query, setQuery] = useState("")
+  const [enabled, setEnabled] = useState(false)
 
   useEffect(() => {
     ;(async () => {
@@ -26,125 +32,94 @@ export default function UserForm() {
   const submit = () => {}
 
   return (
-    <>
+    <div className="m-8">
       <div className="hidden sm:block" aria-hidden="true">
         <div className="py-5">
-          <div className="border-t border-gray-200" />
+          {/* <div className="border-t border-gray-200" /> */}
         </div>
       </div>
 
       <div className="mt-10 sm:mt-0">
         <div className="md:grid md:grid-cols-3 md:gap-6">
-          <div className="mt-5 md:mt-0 md:col-span-full">
-            {/* <form action="#" method="POST"> */}
+          {/* <div className="mt-5 md:mt-0 md:col-span-full">
             <div className="shadow overflow-hidden sm:rounded-md">
               <div className="px-4 py-5 bg-white sm:p-6">
-                <form onSubmit={submit}>
-                  <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-4">
-                      <label
-                        htmlFor="email_address"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        SQL QUERY
-                      </label>
-                      <input
-                        type="text"
-                        name="query"
-                        id="query"
-                        placeholder="SQL Query"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                      />
-                    </div>
+                <div className="grid grid-cols-6 gap-6">
+                  <div className="col-span-6 sm:col-span-4">
+                    <label
+                      htmlFor="email_address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      SQL QUERY
+                    </label>
+                    <input
+                      type="text"
+                      name="query"
+                      id="query"
+                      placeholder="SQL Query"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                    />
+                  </div>
 
-                    <div className="col-span-full sm:col-span-full">
-                      {/* <div className="px-4 py-3 bg-gray-50 text-right sm:px-6"> */}
-                      <button
-                        type="submit"
-                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        Test Query
-                      </button>
-                      {/* </div> */}
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="flex flex-col">
-                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                  <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    {/* <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"> */}
-                    {/* <table className="min-w-full divide-y divide-gray-200"> */}
-                    {/* <thead className="bg-gray-50">
-                            <tr>
-                              <th
-                                scope="col"
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                              >
-                                Name
-                              </th>
-                              <th
-                                scope="col"
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                              >
-                                Title
-                              </th>
-                              <th
-                                scope="col"
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                              >
-                                Email
-                              </th>
-                              <th
-                                scope="col"
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                              >
-                                Role
-                              </th>
-                              <th scope="col" className="relative px-6 py-3">
-                                <span className="sr-only">Edit</span>
-                              </th>
-                            </tr>
-                          </thead> */}
-                    {/* <tbody className="bg-white divide-y divide-gray-200">
-                            {people.map((person) => (
-                              <tr key={person.email}>
-                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{person.name}</td>
-                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.title}</td>
-                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.email}</td>
-                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
-                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                    Edit
-                                  </a>
-                                </td>
-                              </tr>
-                            ))}
-                          </tbody> */}
-                    {/* </table> */}
-                    {/* </div> */}
-                  </div>
-                </div>
-              </div>
-              {/* <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <div className="col-span-full sm:col-span-full">
                     <button
                       type="submit"
                       className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
-                      Save
+                      Test Query
                     </button>
-                  </div> */}
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </div> */}
 
           <div className="md:col-span-full">
             {/* <form action="#" method="POST"> */}
             <div className="shadow overflow-hidden sm:rounded-md">
               <div className="px-4 py-5 bg-white sm:p-6">
+                <div className="grid grid-cols-6 gap-6 ">
+                  <div className="col-span-6 sm:col-span-4 mt-5">
+                    <label
+                      htmlFor="email_address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Name of the Workflow
+                    </label>
+                    <input
+                      type="text"
+                      name="query"
+                      id="query"
+                      placeholder="Name.."
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-6 gap-6 ">
+                  <div className="col-span-6 sm:col-span-4 mt-5">
+                    <label
+                      htmlFor="email_address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      SQL Query
+                    </label>
+                    <input
+                      type="text"
+                      name="query"
+                      id="query"
+                      placeholder="SQL Query"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      value={query}
+                      onChange={(e) => setQuery(e.target.value)}
+                    />
+                  </div>
+                </div>
                 <div className="grid grid-cols-6 gap-6">
-                  <div className="col-span-6 sm:col-span-4">
+                  {/* <div className="col-span-6 sm:col-span-4">
                     <label
                       htmlFor="email_address"
                       className="block text-sm font-medium text-gray-700"
@@ -159,10 +134,10 @@ export default function UserForm() {
                       placeholder="INPUT"
                       className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     />
-                  </div>
+                  </div> */}
 
                   {/* SELECT SALESFORCE FLOWS */}
-                  <div className="col-span-6 sm:col-span-3">
+                  <div className="col-span-6 sm:col-span-3 mt-5">
                     <label
                       htmlFor="country"
                       className="block text-sm font-medium text-gray-700"
@@ -181,7 +156,35 @@ export default function UserForm() {
                     </select>
                   </div>
                 </div>
+                <div className="col-span-6 sm:col-span-3 mt-5">
+                  <Switch.Group as="div" className="flex items-center">
+                    <Switch
+                      checked={enabled}
+                      onChange={setEnabled}
+                      className={classNames(
+                        enabled ? "bg-indigo-600" : "bg-gray-200",
+                        "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      )}
+                    >
+                      <span className="sr-only">Use setting</span>
+                      <span
+                        aria-hidden="true"
+                        className={classNames(
+                          enabled ? "translate-x-5" : "translate-x-0",
+                          "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
+                        )}
+                      />
+                    </Switch>
+                    <Switch.Label as="span" className="ml-3">
+                      <span className="text-sm font-medium text-gray-900">
+                        Pause Automation{" "}
+                      </span>
+                    </Switch.Label>
+                  </Switch.Group>
+                </div>
               </div>
+
+              {/* Dont touch anything below this point */}
               <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                 <button
                   type="submit"
@@ -195,6 +198,6 @@ export default function UserForm() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
