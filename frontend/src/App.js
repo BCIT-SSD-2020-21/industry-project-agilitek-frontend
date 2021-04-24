6c288ece40b5ee3ffbcc616ec69a84d774621344
Gökay Abay <abaygokay@gmail.com>
2021-04-24 12:53:58 -0700

    Routes reararranged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from "react"
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import UserDash from "./components/Landing/userDash"
 import CreateWorkflow from "./Pages/CreateWorkflow"
 import Landing from "./Pages/Landing"
+import WorkflowForm from "./components/WorkflowForm/index"
+import WorkflowTable from "./components/WorkflowTable/WorkflowTable"
 
 function App() {
   return (
@@ -11,10 +14,10 @@ function App() {
         {/* <div className="bg-gray-800 pb-32"> */}
         <Switch>
           <Route exact path="/create">
-            <CreateWorkflow />
+            <UserDash children={<WorkflowForm />} />
           </Route>
           <Route exact path="/">
-            <Landing />
+            <UserDash children={<WorkflowTable />} />
           </Route>
         </Switch>
         {/* </div> */}
