e9df5a9826eafce37ae2e48fc41e93e6e85b6636
Gökay Abay <abaygokay@gmail.com>
2021-04-26 09:02:37 -0700

    Change configure button colour

--- frontend/src/components/WorkflowTable/WorkflowTable.js
+++ frontend/src/components/WorkflowTable/WorkflowTable.js
@@ -128,7 +128,7 @@ const WorkflowTable = () => {
                             </span>
                             {workflow.currency}
                           </td>
-                          <td className="hidden px-6 py-4 whitespace-nowrap text-sm text-gray-500 md:block">
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                             <span
                               className={classNames(
                                 statusStyles[workflow.active],
@@ -141,7 +141,7 @@ const WorkflowTable = () => {
                           <td className="px-6 py-4 text-right whitespace-nowrap text-sm text-gray-500">
                             <button
                               type="button"
-                              className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                              className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
                               onClick={() =>
                                 history.push(`/configure/${workflow.id}`)
                               }
