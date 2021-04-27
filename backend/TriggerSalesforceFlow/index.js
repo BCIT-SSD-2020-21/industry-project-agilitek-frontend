0355668ef0a9a611088c4e87b13e62e6e0424596
Steven <clai86@my.bcit.ca>
2021-04-27 14:15:41 -0700

    Modified triggerSalesforceFlow lambda so that it accepts sObject

--- backend/TriggerSalesforceFlow/index.js
+++ backend/TriggerSalesforceFlow/index.js
@@ -3,15 +3,17 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
     // Getting the body
-    const { flowUrl, inputs } = event;
-    var label = '';
+    const { flowUrl, resultSet, query } = event.workflowData;
+    const { index, step } = event.iterator;
+    var result;
 
     try {
         // Getting the OAuth token
         const OAuthRes = await OAuth();
         const OAuthData = await OAuthRes.getOAuthToken();
 
-        // Get flow label
+        // Get request to Salesforce API
+        // Get the label from the inputs field
         const getLabelResponse = await fetch(
             `${process.env.INSTANCE_URL}${flowUrl}`,
             {
@@ -23,23 +25,37 @@ exports.handler = async (event) => {
         );
         const getLabelData = await getLabelResponse.json();
 
-        // Check if label exists
-        // If exists then assign the label
-        // Else return an error message
-        if (
-            getLabelData &&
-            getLabelData.inputs[0] &&
-            getLabelData.inputs[0].label
-        ) {
-            label = getLabelData.inputs[0].label;
+        const label = getLabelData.inputs[0].label;
+
+        // Check if the workflow's inputs is a sObject type
+        // TRUE: Set sObjectType variable
+        // FALSE: Set result variable to the Id string
+        if (getLabelData.inputs[0].type === 'SOBJECT') {
+            let sObjectType = getLabelData.inputs[0].sobjectType;
+
+            // Check if the resultSet has more than one item
+            // TRUE: set the result to an array of objects with attributes
+            // field and all the columns from the SalesforceContacts
+            // table
+            // FALSE: set result to an object with attributes field
+            // and all the columns from the SalesforceContacts table
+            if (resultSet.length > 1) {
+                result = [];
+
+                resultSet.forEach((input, index) => {
+                    result.push({
+                        attributes: { type: sObjectType },
+                        ...resultSet[index],
+                    });
+                });
+            } else {
+                result = {
+                    attributes: { type: sObjectType },
+                    ...resultSet[0],
+                };
+            }
         } else {
-            return {
-                statusCode: 400,
-                'Access-Control-Allow-Origin': '*',
-                body: JSON.stringify({
-                    errorMessage: 'Label not found',
-                }),
-            };
+            result = resultSet[0].Id;
         }
 
         // Post request to Salesforce API
@@ -54,7 +70,7 @@ exports.handler = async (event) => {
                 body: JSON.stringify({
                     inputs: [
                         {
-                            [label]: inputs,
+                            [label]: result,
                         },
                     ],
                 }),
@@ -63,11 +79,15 @@ exports.handler = async (event) => {
         const postData = await postResponse.json();
 
         return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
+            iterator: {
+                index,
+                step,
             },
-            body: JSON.stringify(postData),
+            body: JSON.stringify({
+                triggered_response: postData,
+                sql_query: query,
+                flow_url: flowUrl,
+            }),
         };
     } catch (error) {
         throw Error(`Error while requesting Salesforce API: ${error}`);
