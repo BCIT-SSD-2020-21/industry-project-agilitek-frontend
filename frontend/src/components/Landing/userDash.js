f050aa9a5e3729b6f9b33276c5b7c314b698d853
Gökay Abay <abaygokay@gmail.com>
2021-04-29 11:26:52 -0700

    Delete btn removed

--- frontend/src/components/Landing/userDash.js
+++ frontend/src/components/Landing/userDash.js
@@ -390,17 +390,17 @@ export default function UserDash({ children, page }) {
                     <>
                       <button
                         type="button"
-                        className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                        className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
                         onClick={() => history.push("/create")}
                       >
                         Add Workflow
                       </button>
-                      <button
+                      {/* <button
                         type="button"
                         className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
                       >
                         Delete Workflow
-                      </button>
+                      </button> */}
                     </>
                   ) : (
                     <button
