e1d9edd65fcf12171a3260a651481cb0abe42f08
Gökay Abay <abaygokay@gmail.com>
2021-04-29 11:26:03 -0700

    Cancel btn added

--- frontend/src/components/WorkflowForm/WorfkflowForm.js
+++ frontend/src/components/WorkflowForm/WorfkflowForm.js
@@ -230,6 +230,13 @@ export default function UserForm() {
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                   <button
                     type="submit"
+                    onClick={() => history.push("/")}
+                    className="mx-6 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
                     onClick={disableButton}
                     style={{ backgroundColor: processsing && "grey" }}
                     className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
