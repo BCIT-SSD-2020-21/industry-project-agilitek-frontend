fd1797520061f71e1c2e952437719ba3e9a056e7
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-30 13:32:32 -0700

    removed button code

--- frontend/src/components/WorkflowDetails/WorkflowDetail.js
+++ frontend/src/components/WorkflowDetails/WorkflowDetail.js
@@ -173,32 +173,6 @@ const { name, desc, flowUrl, query, active } = formData
                       <div className="grid grid-cols-6 gap-6">
 
                       </div>
-                      <div className="col-span-6 sm:col-span-3 mt-5">
-                        <Switch.Group as="div" className="flex items-center">
-                          <Switch
-                            checked={active}
-                            onChange={handleSwitch}
-                            className={classNames(
-                              active ? "bg-indigo-600" : "bg-gray-200",
-                              "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            )}
-                          >
-                            <span className="sr-only">Use setting</span>
-                            <span
-                              aria-hidden="true"
-                              className={classNames(
-                                active ? "translate-x-5" : "translate-x-0",
-                                "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-                              )}
-                            />
-                          </Switch>
-                          <Switch.Label as="span" className="ml-3">
-                            <span className="text-sm font-medium text-gray-900">
-                              {active ? "Automation active" : "Automation paused"}
-                            </span>
-                          </Switch.Label>
-                        </Switch.Group>
-                      </div>
                     </div>
 
                   </div>
