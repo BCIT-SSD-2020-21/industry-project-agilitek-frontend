8d96e3aa31ad1356bfbef140a1316a9ca82ece4f
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-30 16:28:17 -0700

    added padding

--- frontend/src/components/WorkflowForm/WorfkflowForm.js
+++ frontend/src/components/WorkflowForm/WorfkflowForm.js
@@ -553,19 +553,19 @@ export default function UserForm() {
                                 {/* Dont touch anything below this point */}
                                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                                     <button
-                                      type="submit"
-                                      onClick={handleDelete}
-                                      style={{ backgroundColor: processsing && "grey" }}
-                                      className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-                                    >
-                                    Delete Configuration
-                                    </button>
-                                    <button
                                         type="button"
                                         onClick={() => history.push('/')}
                                         className="mx-6 inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
                                     >
                                         Cancel
+                                    </button>
+                                    <button
+                                      type="submit"
+                                      onClick={handleDelete}
+                                      style={{ backgroundColor: processsing && "grey" }}
+                                      className="mr-6 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                                    >
+                                    Delete Configuration
                                     </button>
                                     <button
                                         type="submit"
