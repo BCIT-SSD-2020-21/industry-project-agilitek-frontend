93cab32d9519e2c450bde0893615830ea9aa40a6
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-05-04 11:17:17 -0700

    cleaned up code

--- frontend/src/components/WorkflowDetails/WorkflowDetail.js
+++ frontend/src/components/WorkflowDetails/WorkflowDetail.js
@@ -7,7 +7,6 @@ import {
   getDBTables,
   getMetadata,
 } from "../../api/network"
-import { Switch } from "@headlessui/react"
 import { useHistory, useParams } from "react-router-dom"
 import ExecutionTable from "../ExecutionTable/ExecutionTable"
 import MappingInput from '../WorkflowForm/MappingInput';
@@ -31,17 +30,12 @@ export default function WorkflowDetail() {
 const history = useHistory()
 const { id } = useParams()
 
-
 const [dbTables, setDbTables] = useState([])
 const [workflows, setWorkflows] = useState([])
-const [dbColumns, setDBColumns] = useState([])
-const [detailInputs, setdetailInputs] = useState([])
-
 const [sfMetadata, setSfMetadata] = useState([])
-const [mappings, setMappings] = useState([])
 const [tempMapping, setTempMapping] = useState({})
-
 const [options, setOptions] = useState([])
+
 const [formData, setFormData] = useState({
   name: "",
   desc: "",
@@ -71,6 +65,7 @@ useEffect(() => {
 
       // If there is an id in the url parameters then retrieve all
       // the data for that workflow
+      try{
       if (id) {
         const res = await getWorkflow(id)
         // console.log(res)
@@ -89,7 +84,6 @@ useEffect(() => {
           runAgain: res.run_again,
           mapping: res.mapping,
         })
-
         // Fetch and set Salesforce metadata
         if (res.type === "SOBJECT") {
           const metadataRes = await getMetadata(res.sobjecttype)
@@ -97,10 +91,10 @@ useEffect(() => {
         }
         setTempMapping(res.mapping)
       }
+    } catch(err) {
+      history.push('/404')
+    }
     })()
-    // } catch(error){
-    //   history.push('/404')
-    // }
   }, [])
 
 // Makes a call to db to return query results
@@ -129,150 +123,132 @@ const {
   mapping,
 } = formData
 
-// useEffect(() => {
-//   // console.log(mapping)
-//   for (const key in mapping) {
-//     setdetailInputs(...detailInputs, <DetailInput mapping={mapping}></DetailInput>)
-//     // console.log(`${key} : ${mapping[key]}`)
-//   }
-//   }, [])
-
-    return (
-        <div className="m-8">
-          <div className="hidden sm:block" aria-hidden="true">
-            <div className="py-2">
-            </div>
-          </div>
-          <div className="mt-10 sm:mt-0">
-            <div className="md:grid md:grid-cols-3 md:gap-6">
-              <div className="md:col-span-full">
-                <form onSubmit={submit}>
-                  <div className="shadow overflow-hidden sm:rounded-md">
+return (
+  <div className="m-8">
+     <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-2">
+        </div>
+     </div>
+     <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+           <div className="md:col-span-full">
+              <form onSubmit={submit}>
+                 <div className="shadow overflow-hidden sm:rounded-md">
                     <div className="px-4 py-5 bg-white sm:p-6">
-                      <div className="grid grid-cols-6 gap-6 ">
-                        <div className="col-span-6 sm:col-span-4 mt-5">
-                          <label
-                            htmlFor="email_address"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            Name of the Workflow
-                          </label>
-                          <input
-                            disabled
-                            type="text"
-                            name="name"
-                            id="name"
-                            placeholder="Name.."
-                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            value={name}
-                          />
-                        </div>
-                      </div>
-                    <>
-                      <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-                        <div className="sm:col-span-3">
-                          <label
-                            htmlFor="inputs_type"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            Salesforce Flow Inputs Type
-                          </label>
-                          <input
-                            type="text"
-                            name="inputs_type"
-                            id="inputs_type"
-                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            value={
-                              sObjectType
-                                ? `${label} (${type} of Type=${sObjectType})`
-                                : `${label} (Type=${type})`
-                            }
-                            disabled
-                          />
-                        </div>
-                        <div>
-                        </div>
-                      </div>
-                      {/* Salesforce MetaData field */}
-                      {/* {detailInputs}   */}
-                      
-                      {mapping ? (
-                      
-                      
-                      Object.keys(mapping).map((key, i) => (
-                        
-                        <DetailInput key={i} mapping={mapping[key]} mappingKey={key} />
-                      ))
-                      ) : ( null)   
-                      }
-                      {/* Mapping dropdown lists */}
-                      {sObjectType ? (
-                        <div className="mappings">{formData.mapping[0]}</div>
-                      ) : null}
-                      <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-4 mt-5">
-                          <label
-                            htmlFor="where_clause"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            WHERE Clause
-                          </label>
-                          <input
-                            type="text"
-                            name="whereClause"
-                            id="where_clause"
-                            placeholder="Enter WHERE clause..."
-                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            value={whereClause}
-                            disabled
-                          />
-                        </div>
-                      </div>
-                    </>
-                      <div className="grid grid-cols-6 gap-6 ">
-                        <div className="col-span-6 sm:col-span-4 mt-5">
-                          <label
-                            htmlFor="email_address"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            Automation Status
-                          </label>
-                          <input
-                            disabled
-                            type="text"
-                            name="query"
-                            id="query"
-                            placeholder="SQL Query"
-                            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            value={active}
-                          />
-                        </div>
-                      </div>
-                      <div className="grid grid-cols-6 gap-6">
-
-                      </div>
+                       <div className="grid grid-cols-6 gap-6 ">
+                          <div className="col-span-6 sm:col-span-4 mt-5">
+                             <label
+                                htmlFor="email_address"
+                                className="block text-sm font-medium text-gray-700"
+                                >
+                             Name of the Workflow
+                             </label>
+                             <input
+                                disabled
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Name.."
+                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                value={name}
+                                />
+                          </div>
+                       </div>
+                       <>
+                       <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                          <div className="sm:col-span-3">
+                             <label
+                                htmlFor="inputs_type"
+                                className="block text-sm font-medium text-gray-700"
+                                >
+                             Salesforce Flow Inputs Type
+                             </label>
+                             <input
+                             type="text"
+                             name="inputs_type"
+                             id="inputs_type"
+                             className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                             value={
+                             sObjectType
+                             ? `${label} (${type} of Type=${sObjectType})`
+                             : `${label} (Type=${type})`
+                             }
+                             disabled
+                             />
+                          </div>
+                          <div>
+                          </div>
+                       </div>
+                       {/* Salesforce MetaData field */}
+                       {/* {detailInputs}   */}
+                       {mapping ? (
+                       Object.keys(mapping).map((key, i) => (
+                       <DetailInput key={i} mapping={mapping[key]} mappingKey={key} />
+                       ))
+                       ) : ( null)   
+                       }
+                       {/* Mapping dropdown lists */}
+                       {sObjectType ? (
+                       <div className="mappings">{formData.mapping[0]}</div>
+                       ) : null}
+                       <div className="grid grid-cols-6 gap-6">
+                          <div className="col-span-6 sm:col-span-4 mt-5">
+                             <label
+                                htmlFor="where_clause"
+                                className="block text-sm font-medium text-gray-700"
+                                >
+                             WHERE Clause
+                             </label>
+                             <input
+                                type="text"
+                                name="whereClause"
+                                id="where_clause"
+                                placeholder="Enter WHERE clause..."
+                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                value={whereClause}
+                                disabled
+                                />
+                          </div>
+                       </div>
+                       </>
+                       <div className="grid grid-cols-6 gap-6 ">
+                          <div className="col-span-6 sm:col-span-4 mt-5">
+                             <label
+                                htmlFor="email_address"
+                                className="block text-sm font-medium text-gray-700"
+                                >
+                             Automation Status
+                             </label>
+                             <input
+                                disabled
+                                type="text"
+                                name="query"
+                                id="query"
+                                placeholder="SQL Query"
+                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                value={active}
+                                />
+                          </div>
+                       </div>
+                       <div className="grid grid-cols-6 gap-6">
+                       </div>
                     </div>
-
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-
-          <div className="hidden sm:block" aria-hidden="true">
-            <div className="py-5">
-            </div>
-          </div>
-          
-
-        <div className="mt-10 sm:mt-0">
-            <div className="md:grid md:grid-cols-3 md:gap-6">
-                <div className="md:col-span-full">
-                <ExecutionTable/>
-                </div>
-            </div>
+                 </div>
+              </form>
+           </div>
         </div>
-
+     </div>
+     <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
         </div>
-    )
+     </div>
+     <div className="mt-10 sm:mt-0">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+           <div className="md:col-span-full">
+              <ExecutionTable/>
+           </div>
+        </div>
+     </div>
+  </div>
+  )
 }
