cfe7acb43423a6ec2f33e86806343daf2bb3d298
Steven <clai86@my.bcit.ca>
2021-05-04 11:34:56 -0700

    Clean up frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,36 @@
-import React from "react"
+import React from 'react';
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-import UserDash from "./components/Landing/userDash"
-import CreateWorkflow from "./Pages/CreateWorkflow"
-import Landing from "./Pages/Landing"
-import WorkflowForm from "./components/WorkflowForm/WorfkflowForm"
-import ErrorPage from "./components/ErrorPage/ErrorPage"
-import WorkflowTable from "./components/WorkflowTable/WorkflowTable"
-import WorkflowDetail from "./components/WorkflowDetails/WorkflowDetail"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserDash from './components/Landing/userDash';
+import WorkflowForm from './components/WorkflowForm/WorfkflowForm';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import WorkflowTable from './components/WorkflowTable/WorkflowTable';
+import WorkflowDetail from './components/WorkflowDetails/WorkflowDetail';
 
 function App() {
-  return (
-    <Router>
-      {/* <div className="bg-gray-800 pb-32"> */}
-      <Switch>
-        <Route exact path="/">
-          <UserDash page="main" children={<WorkflowTable />} />
-        </Route>
-        <Route exact path="/create">
-          <UserDash children={<WorkflowForm />} />
-        </Route>
-        <Route exact path="/configure/:id">
-          <UserDash children={<WorkflowForm />} />
-        </Route>
-        <Route exact path="/details/:id">
-          <UserDash children={<WorkflowDetail />} />
-        </Route>
-        <Route exact path="/404">
-        <UserDash children={<ErrorPage />} />
-        </Route>
-      </Switch>
-      {/* </div> */}
-    </Router>
-  )
+    return (
+        <Router>
+            {/* <div className="bg-gray-800 pb-32"> */}
+            <Switch>
+                <Route exact path="/">
+                    <UserDash page="main" children={<WorkflowTable />} />
+                </Route>
+                <Route exact path="/create">
+                    <UserDash children={<WorkflowForm />} />
+                </Route>
+                <Route exact path="/configure/:id">
+                    <UserDash children={<WorkflowForm />} />
+                </Route>
+                <Route exact path="/details/:id">
+                    <UserDash children={<WorkflowDetail />} />
+                </Route>
+                <Route exact path="/404">
+                    <UserDash children={<ErrorPage />} />
+                </Route>
+            </Switch>
+            {/* </div> */}
+        </Router>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/components/ExecutionTable/ExecutionTable.js
+++ frontend/src/components/ExecutionTable/ExecutionTable.js
@@ -1,98 +1,90 @@
-import React, { useState, useEffect } from "react";
-import { getWorkflowLogs } from "../../api/network";
-import { useHistory, useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { getWorkflowLogs } from '../../api/network';
+import { useParams } from 'react-router-dom';
 
 export default function ExecutionTable() {
-  const history = useHistory();
-  const { id } = useParams();
+    const { id } = useParams();
 
-  const [logArray, setLogArray] = useState([]);
+    const [logArray, setLogArray] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      if (id) {
-        const res = await getWorkflowLogs(id);
-        console.log(res);
-        setLogArray(res);
-      }
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            if (id) {
+                const res = await getWorkflowLogs(id);
+                setLogArray(res);
+            }
+        })();
 
-  return (
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    WorkFlow Id
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    WorkFlow Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Time Executed
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Record Id
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {
-                  // logArray[0].action_name ? (
-                  logArray.map((logs) => (
-                    <tr>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {logs.workflow_id}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {logs.action_name}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {new Date(
-                          logs.time_of_completion
-                        ).toLocaleString("en-US", { timeZone: "UTC" })}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        {logs.record_id}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"></td>
-                    </tr>
-                  ))
-                  // ) : (
-                  //     <tr>
-                  //     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">NULL</td>
-                  //     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">NULL</td>
-                  //     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">NULL</td>
-                  //     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">NULL</td>
-                  //     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  //     </td>
-                  //  </tr>
-                  // )
-                }
-              </tbody>
-            </table>
-          </div>
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        WorkFlow Id
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        WorkFlow Name
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Time Executed
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                    >
+                                        Record Id
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="relative px-6 py-3"
+                                    >
+                                        <span className="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {logArray.map((logs) => (
+                                    <tr>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            {logs.workflow_id}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {logs.action_name}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {new Date(
+                                                logs.time_of_completion
+                                            ).toLocaleString('en-US', {
+                                                timeZone: 'UTC',
+                                            })}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {logs.record_id}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
