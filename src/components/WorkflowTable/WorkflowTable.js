19e9fa546efe62b33bab2de8acc974f60a837b09
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-05-05 16:04:43 -0700

    Fixed Issue with Edit Display

--- src/components/WorkflowTable/WorkflowTable.js
+++ src/components/WorkflowTable/WorkflowTable.js
@@ -134,7 +134,7 @@ const WorkflowTable = (search) => {
                                                 <th className="hidden px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider md:block">
                                                     Status
                                                 </th>
-                                                <th className="px-6 py-3 bg-gray-50 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                <th className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                     Edit
                                                 </th>
                                             </tr>
