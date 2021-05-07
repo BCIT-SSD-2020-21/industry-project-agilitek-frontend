a92528e0d255aebfe2b2e4355f1ee0f2ec846bea
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-05-06 23:41:18 -0700

    Sucessfully different display onlogin

--- src/components/Landing/userDash.js
+++ src/components/Landing/userDash.js
@@ -292,11 +292,11 @@ export default function UserDash({ children, page }) {
                                     <>
                                         <div>
                                             <Menu.Button className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50">
-                                                {/* <img
+                                                <img
                                                     className="h-8 w-8 rounded-full"
                                                     src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Vancouver_Whitecaps_FC_logo.svg/180px-Vancouver_Whitecaps_FC_logo.svg.png"
                                                     alt=""
-                                                /> */}
+                                                />
                                                 <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
                                                     <span className="sr-only">
                                                         Open user menu for{' '}
@@ -369,21 +369,6 @@ export default function UserDash({ children, page }) {
                                                         </a>
                                                     )}
                                                 </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a onClick={() => loginWithRedirect() }
-                                                            href="/"
-                                                            className={classNames(
-                                                                active
-                                                                    ? 'bg-gray-100'
-                                                                    : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Login
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
                                             </Menu.Items>
                                         </Transition>
                                     </>
@@ -395,100 +380,14 @@ export default function UserDash({ children, page }) {
                                 <>
                                     <div>
                                         <Menu.Button className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50">
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Vancouver_Whitecaps_FC_logo.svg/180px-Vancouver_Whitecaps_FC_logo.svg.png"
-                                                alt=""
-                                            />
-                                            <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
-                                                <span className="sr-only">
+                                            <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block" onClick={() => loginWithRedirect()}>
+                                                <span className="sr-only" >
                                                     Open user menu for{' '}
                                                 </span>
                                                 Login
                                             </span>
-                                            {/* <ChevronDownIcon
-                                                className="hidden flex-shrink-0 ml-1 h-5 w-5 text-gray-400 lg:block"
-                                                aria-hidden="true"
-                                            /> */}
                                         </Menu.Button>
                                     </div>
-                                    {/* <Transition
-                                        show={open}
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <Menu.Items
-                                            static
-                                            className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                        >
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="/"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="/"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        onClick={() => logout({ returnTo: window.location.origin })}
-                                                        href="/"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Logout
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a onClick={() => loginWithRedirect() }
-                                                        href="/"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Login
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition> */}
                                 </>
                             )}
                         </Menu>
