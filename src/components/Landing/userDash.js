30d322a66070763e2831513555aae218d6833195
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-05-07 14:33:54 -0700

    cleaned up code

--- src/components/Landing/userDash.js
+++ src/components/Landing/userDash.js
@@ -481,12 +481,6 @@ export default function UserDash({ children, page }) {
                                             >
                                                 Add Workflow
                                             </button>
-                                            {/* <button
-                        type="button"
-                        className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
-                      >
-                        Delete Workflow
-                      </button> */}
                                         </>
                                     ) : (
                                         <button
