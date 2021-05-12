92750f1ea535a24227bf5b8b141df44144ea167f
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-05-12 15:06:32 -0700

    modified auth provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-    <Auth0Provider
-      domain="agilitek.us.auth0.com"
-      clientId="Pn08OIuU2lNszAFLD4WZlelkOIdhwmVu"
-      redirectUri="http://localhost:3000/"
-      returnTo="http://localhost:3000/"
-      audience="https://agilitek.us.auth0.com/api/v2/"
-      scope="read:current_user update:current_user_metadata"
-    >
+<Auth0Provider
+    domain="agilitek.us.auth0.com"
+    clientId="Pn08OIuU2lNszAFLD4WZlelkOIdhwmVu"
+    redirectUri={
+      process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:3000/'
+        : 'https://agilitek-fan-data-platform.herokuapp.com/'
+    }
+    returnTo={
+      process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:3000/'
+        : 'https://agilitek-fan-data-platform.herokuapp.com/'
+    }
+    audience="https://agilitek.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
       <App />
     </Auth0Provider>,
     document.getElementById("root")
